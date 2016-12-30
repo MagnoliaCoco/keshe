1f64c4225bd352d827bbd665eba0141e6fe79927
aniefefalas <aniefefalas@outlook.com>
2016-12-30 20:18:54 +0800

    init

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 let express = require('express');
 let mysql = require('mysql');
-let ejs = require('ejs');
+let react = require('react');
+let reactdom = require('react-dom');
 let config = require('./config');
 
-var connection = mysql.createConnection(config.mysql);
 
+var connection = mysql.createConnection(config.mysql);
 connection.connect(function(err) {
-    if (err) {
-        console.error(err);
-    }
+    if (err) console.error(err);
 });
 
+connection.query('select * from user', function(err, rows) {
+    if (err) throw err;
 
+});
 
 let app = express();
 app.get('/', function(req, res) {
