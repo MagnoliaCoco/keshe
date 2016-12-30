6da4f74c0d4907017c43b20cc00d5dee1b6644d5
aniefefalas <aniefefalas@outlook.com>
2016-12-30 19:13:35 +0800

    init

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 let express = require('express');
 let mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'rainylu1',
-    database: 'keshe'
-});
+let ejs = require('ejs');
+let config = require('./config');
+
+var connection = mysql.createConnection(config.mysql);
 
 connection.connect(function(err) {
     if (err) {
         console.error(err);
     }
 });
+
+
 
 let app = express();
 app.get('/', function(req, res) {
